c0036763851afddf4d9817a252daee41172f42d4
parvej <parvej1114@yahoo.com>
2020-02-15 07:35:48 +0800

    1st commit

---
+++ src/intro_to_java/Main.java
@@ -0,0 +1,8 @@
+package intro_to_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Introduction to java programming by Daniel Liang 11th edition");
+    }
+}
